be57c938ccd88c90c00e8cdadbe4bb8316e24891
Jan Banda <janbanda95@gmail.com>
2020-11-24 22:24:06 +0100

    random code om workshopgod Jeroen tevreden te houden

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+rightButton = document.getElementById("js--right-button");
+wrongButton = document.getElementById("js--wrong-button");
+
+rightButton.addEventListener(click, (e) =>{
+    console.log("You clicked right!");
+})
+
+wrongButton.addEventListener(click, (e)=>{
+    console.log("Listen here you obstinate piece of shit, I know you can read so get your shit together so I can go to bed. Idiot.");
+})
